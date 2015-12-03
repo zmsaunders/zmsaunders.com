3eb7d8e8347391b52340d83ee8d34b62d43fedbe
Zach Saunders <zsaunders@omnispear.com>
2015-12-03 17:18:01 -0500

    Blog styling

--- resources/views/postList.blade.php
+++ resources/views/postList.blade.php
@@ -1,11 +1,25 @@
 @extends('templates.main')
 
 @section('content')
-    <div class="container">
+    <div class="container postList">
+        <h1>&lt;blog/&gt;</h1>
         @foreach($posts as $post)
-            <h2><a href="#">{{$post->title}}</a></h2>
+            <div class="post">
+            <h2><a href="{{URL::action('BlogController@entry', $post->slug)}}">{{$post->title}}</a></h2>
             <span class="info">{{$post->user->name}} on {{date('M d, Y', strtotime($post->created_at))}}</span>
+            </div>
         @endforeach
     </div>
-    {!! $posts->render() !!}
+
+    {{-- Pagination --}}
+    @if($posts->perPage() < $posts->total())
+        <div class="pages container">
+        @if($posts->currentPage() !== 1)
+            <a class="prev" href="{{ $posts->previousPageUrl() }}"><i class="fa fa-angle-left"></i> Previous</a>
+        @endif
+        @if($posts->currentPage() != $posts->lastPage())
+            <a class="next" href="{{ $posts->nextPageUrl() }}">Next <i class="fa fa-angle-right"></i></a>
+        @endif
+        </div>
+    @endif
 @stop
