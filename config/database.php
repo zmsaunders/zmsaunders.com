df1ba480261bf292586561a3da311f103a97c790
Zach Saunders <zsaunders@omnispear.com>
2015-12-10 12:04:21 -0500

    DB Config

--- config/database.php
+++ config/database.php
@@ -52,6 +52,12 @@ return [
             'prefix'   => '',
         ],
 
+        'testDb' => [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ],
+
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
